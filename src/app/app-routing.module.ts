3038ceaec9e263112a9c799f1926bfdb0acaada9
Sampathkumar_Samiappan_10528 <sampathkumar-samiappan>
2022-11-15 15:48:58 +0530

    others respondent component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GovernmentRespondentComponent } from './forms/highcourt-cases/government-respondent/government-respondent.component';
+import { OthersRespondentComponent } from './forms/highcourt-cases/others-respondent/others-respondent.component';
 
 const routes: Routes = [
-  { path: 'government-respondent-hight-court-cases', component: GovernmentRespondentComponent }
+  { path: 'government-respondent-hight-court-cases', component: GovernmentRespondentComponent },
+  { path: 'others-respondent', component:OthersRespondentComponent}
 ];
 
 @NgModule({
