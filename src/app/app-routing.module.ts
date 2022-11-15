bb57f0b5717c7d48c08bd877c5467f555c7fcece
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-15 11:01:02 +0530

    first form -> government repondent high court cases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GovernmentRespondentComponent } from './forms/highcourt-cases/government-respondent/government-respondent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'government-respondent-hight-court-cases', component: GovernmentRespondentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
