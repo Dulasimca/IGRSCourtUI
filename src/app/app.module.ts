728f2c88833f229b6134b0ff99d3a3e98af5f23c
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-15 10:57:27 +0530

    first form -> government repondent high court cases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GovernmentRespondentComponent } from './government-respondent/government-respondent.component';
+import { GovernmentRespondentComponent } from './forms/highcourt-cases/government-respondent/government-respondent.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,15 @@ import { GovernmentRespondentComponent } from './government-respondent/governmen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    PanelModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forms/highcourt-cases/government-respondent/government-respondent.component.ts
+++ src/app/forms/highcourt-cases/government-respondent/government-respondent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-government-respondent',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./government-respondent.component.scss']
 })
 export class GovernmentRespondentComponent implements OnInit {
-
+  zoneOptions: SelectItem[] = [];
+  zone: any;
+  districtOptions: SelectItem[] = [];
+  district: any;
+  sroOptioins: SelectItem[] = [];
+  sro: any;
+  caseTypeOptions: SelectItem[] = [];
+  caseType: any;
+  caseNo: any;
+  highCourtNameOptions: SelectItem[] = [];
+  highCourtName: any;
+  stateOfCaseOptions: SelectItem[] = [];
+  stateOfCase: any;
+  petitionerName: any;
+  respondentsName: any;
+  gistOfCase: any;
+  selectedValue: string = '1';
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSave() { }
 
 }
