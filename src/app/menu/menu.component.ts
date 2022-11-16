c3ee4be883e0dc34a2f599671f51ba19c081e7b0
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-16 16:47:15 +0530

    menu for both mob & web

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -15,77 +15,31 @@ export class MenuComponent implements OnInit {
     ngOnInit() {
         this.items = [
             {
-                label: 'File',
-                icon: 'pi pi-pw pi-file',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'User', icon: 'pi pi-fw pi-user-plus'},
-                            {label: 'Filter', icon: 'pi pi-fw pi-filter'}
-                        ]
-                    },
-                    {label: 'Open', icon: 'pi pi-fw pi-external-link'},
-                    {separator: true},
-                    {label: 'Quit', icon: 'pi pi-fw pi-times'}
-                ]
+                label: 'Dashboard',
+                icon: 'pi pi-pw pi-home',
             },
             {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
+                label: 'Forms',
+                icon: 'pi pi-fw pi-file-o',
                 items: [
-                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+                    { label: 'High Court Cases', 
+                  items: [
+                    {label: 'Government Respondent', routerLink: '/government-respondent-hight-court-cases'},
+                    {label: 'IGR Respondent', routerLink: '/igr-respondent'},
+                    {label: 'Other Respondent', routerLink: '/others-respondent'},
+                    {label: 'Time Bound/Judgements', routerLink: '/time-bound'},
+                    {label: 'Pending Enquiry', routerLink: '/pending-enquiry-hight-court-cases'},
+                    {label: 'Writ Appeals', routerLink: '/writ-appeals-hight-court-cases'}
+                  ]},
+                  { label: 'Supreme Court Case Details', routerLink: '/supreme-court-case-details'},
+                  {label: 'Law Officers Opinion Register', routerLink: '/lawofficers-opinion-register'},
+
                 ]
             },
             {
                 label: 'Help',
                 icon: 'pi pi-fw pi-question',
-                items: [
-                    {
-                        label: 'Contents',
-                        icon: 'pi pi-pi pi-bars'
-                    },
-                    {
-                        label: 'Search', 
-                        icon: 'pi pi-pi pi-search', 
-                        items: [
-                            {
-                                label: 'Text', 
-                                items: [
-                                    {
-                                        label: 'Workspace'
-                                    }
-                                ]
-                            },
-                            {
-                                label: 'User',
-                                icon: 'pi pi-fw pi-file',
-                            }
-                    ]}
-                ]
             },
-            {
-                label: 'Actions',
-                icon: 'pi pi-fw pi-cog',
-                items: [
-                    {
-                        label: 'Edit',
-                        icon: 'pi pi-fw pi-pencil',
-                        items: [
-                            {label: 'Save', icon: 'pi pi-fw pi-save'},
-                            {label: 'Update', icon: 'pi pi-fw pi-save'},
-                        ]
-                    },
-                    {
-                        label: 'Other',
-                        icon: 'pi pi-fw pi-tags',
-                        items: [
-                            {label: 'Delete', icon: 'pi pi-fw pi-minus'}
-                        ]
-                    }
-                ]
-            }
         ];
     }
 }
