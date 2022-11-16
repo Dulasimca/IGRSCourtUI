1707c7cfd20af2b2c8762abb88904a81fbd9487d
dhanarthi <arthivenkat87@gmail.com>
2022-11-16 13:03:33 +0530

    Adding changes to formIV

--- src/app/forms/highcourt-cases/government-respondent/government-respondent.component.ts
+++ src/app/forms/highcourt-cases/government-respondent/government-respondent.component.ts
@@ -28,7 +28,7 @@ export class GovernmentRespondentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.sroOptioins = [{ label: '-select-', value: 0 },
+    this.sroOptions = [{ label: '-select-', value: 0 },
   { label: '1', value: 1},
 { label: '2', value: 2}];
   }
