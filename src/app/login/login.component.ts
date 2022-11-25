777790bb5175d35ad03a1a33776bfcff7d3359de
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-25 15:59:29 +0530

    login password hide and show

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -10,6 +10,8 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
   username: any;
   password: any;
+  showPwd: boolean = false;
+  // @ViewChild()
   constructor(private _authService: AuthService, private _router: Router) { }
 
   ngOnInit(): void {
@@ -19,6 +21,26 @@ export class LoginComponent implements OnInit {
   onLogin() {
     this._authService.login();
     this._router.navigate(['/dashboard']);
+  }
+
+  onShowPwd() {
+    // if (password.trim() !== '' && this.password !== null && this.password !== undefined) {
+    //   this.showPwd = !this.showPwd;
+    //   if (type === 0) {
+    //     document.getElementById('pwd')?.setAttribute('value', this.password);
+    //   } else {
+
+    //   }
+    // }
+    var inputValue = (<HTMLInputElement>document.getElementById('pwd'));
+    console.log('in', inputValue)
+    if (inputValue.type === 'password') {
+      inputValue.type = 'text';
+      this.showPwd = !this.showPwd;
+    } else {
+      this.showPwd = !this.showPwd;
+      inputValue.type = 'password';
+    }
   }
 
 
